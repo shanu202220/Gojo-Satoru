fa9aecdd224d89f1e8e2fc40c1e148530a0e3f78
shanu202220 <113174880+shanu202220@users.noreply.github.com>
2022-09-09 15:27:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['+94774702116']
+global.premium = ['+94774702116']
+global.ownernomer = '+94774702116'
+global.ownername = '🐦sAsiNDu'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
